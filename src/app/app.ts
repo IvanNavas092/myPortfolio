863a2b18df9c370d7a0bf13703e1e902035c7053
IvanNavas092 <navass21112@gmail.com>
2025-09-14 14:05:54 +0200

    add animation scroll in div line in hero and move the purple shadow

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
 
+// gsap
+import { gsap } from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+
+gsap.registerPlugin(ScrollSmoother);
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header],
@@ -10,4 +16,15 @@ import { Header } from "./shared/header/header";
 })
 export class App {
   protected readonly title = signal('myPortfolio');
+
+  constructor() {
+    if (typeof window !== 'undefined') { // solo en navegador
+      ScrollSmoother.create({
+        wrapper: '#wrapper', // contenedor principal
+        content: '#content', // contenido que va a scrollear
+        smooth: 0.3,         // cuánto suaviza el scroll
+        effects: true        // si quieres animaciones ligadas al scroll
+      });
+    }
+  }  
 }
