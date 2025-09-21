31400afd46bef632a0b8a6465ecedea442a82480
Ivan Navas <navass21112@gmail.com>
2025-09-21 12:33:15 +0200

    problem with component letters for make responsive the hero

--- src/app/features/home/components/hero/hero.ts
+++ src/app/features/home/components/hero/hero.ts
@@ -1,6 +1,16 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+  OnDestroy,
+  PLATFORM_ID,
+  HostListener,
+  Inject,
+} from '@angular/core';
 import { HoverLetters } from '@/shared/hover-letters/hover-letters';
 import { AnimationService } from '@/core/services/animationService';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
@@ -10,12 +20,63 @@ import { AnimationService } from '@/core/services/animationService';
 export class Hero implements AfterViewInit, OnDestroy {
   @ViewChild('line') line!: ElementRef<HTMLDivElement>;
 
-  constructor(private animationService: AnimationService) {}
+  titleSize: string = '';
+  subtitleSize: string = '';
+
+  constructor(
+    private animationService: AnimationService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  @HostListener('window:resize')
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateSizes();
+    }
+  }
 
   ngAfterViewInit() {
     this.animationService.animateLineOnScroll(this.line.nativeElement);
   }
   ngOnDestroy() {
     this.animationService.killAllScrollTriggers();
+  }
+
+  responsiveTitle(): string {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 640) {
+      // mobile
+      return '150px';
+    } else if (windowWidth < 1024) {
+      // tablets
+      return '450px';
+    } else if (windowWidth < 1280) {
+      // laptop
+      return '500px';
+    } else {
+      // desktop
+      return '500px';
     }
+  }
+
+  responsiveSubtitle(): string {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 640) {
+      // mobile
+      return '100px';
+    } else if (windowWidth < 1024) {
+      // tablets
+      return '350px';
+    } else {
+      // desktop
+      return '400px';
+    }
+  }
+
+  updateSizes() {
+    this.titleSize = this.responsiveTitle();
+    this.subtitleSize = this.responsiveSubtitle();
+  }
 }
