e3c27aac2104067793fb1521c80676159ed717bb
IvanNavas092 <navass21112@gmail.com>
2025-11-07 19:33:12 +0100

    edit projects

--- src/app/features/home/components/projects/projects.ts
+++ src/app/features/home/components/projects/projects.ts
@@ -7,38 +7,51 @@ import { TranslatePipe } from '@ngx-translate/core';
   selector: 'app-projects',
   imports: [BoxProject, NewSection, TranslatePipe],
   templateUrl: './projects.html',
-  styles: ``
+  styles: ``,
 })
 export class Projects {
-
   projects: Project[] = [
     {
       link: 'https://skillping.netlify.app/',
       title: 'SkillPing',
       description: 'desc-skillping',
       image: './projects/skillPing.jpg',
-      utils: ['Angular', 'Django', 'Tailwind', 'PostgreSQL']
+      utils: ['Angular', 'Django', 'Tailwind', 'PostgreSQL'],
     },
     {
       link: 'https://fastsnippets.es',
       title: 'FastSnippets',
       description: 'desc-fastsnippets',
       image: './projects/fastSnippets.jpg',
-      utils: ['Angular', 'Tailwind', 'Firebase']
+      utils: ['Angular', 'Tailwind', 'Firebase'],
+    },
+    {
+      link: 'https://pomodorocount.netlify.app/',
+      title: 'Costa Digital',
+      description: 'desc-costadigital',
+      image: './projects/costaDigital.jpg',
+      utils: ['React', 'Tailwind'],
     },
     {
       link: 'https://pomodorocount.netlify.app/',
       title: 'Pomodoro Count',
       description: 'desc-pomodoro',
       image: './projects/pomodoroCount.jpg',
-      utils: ['Angular', 'Tailwind']
+      utils: ['Angular', 'Tailwind'],
+    },
+    {
+      link: 'https://pomodorocount.netlify.app/',
+      title: 'ELYSE',
+      description: 'desc-elyse',
+      image: './projects/elyse.png',
+      utils: ['React', 'Tailwind'],
     },
     {
       link: '',
       title: 'TimeCapsule',
       description: 'desc-timecapsule',
       image: './projects/timeCapsule.jpg',
-      utils: ['React', 'Tailwind', 'Java Spring', 'PostgreSQL']
-    }
+      utils: ['React', 'Tailwind', 'Java Spring', 'PostgreSQL'],
+    },
   ];
 }
